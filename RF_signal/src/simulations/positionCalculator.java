8f365ff1fedae2ffbf091ca2c351962111277dab
Martin <Martin@Uknown>
2021-05-22 01:00:06 -0500

    adicion de script para calcula la ubicacion del vahiculo

---
+++ RF_signal/src/simulations/positionCalculator.java
@@ -0,0 +1,12 @@
+package simulations;
+
+public class positionCalculator{
+	public static void main(String[]args) {
+		
+	}
+	public static float getPos(int degA, int degB) {
+		int a_ = 1;
+		float a = 0;
+		return a; 
+	}
+}
